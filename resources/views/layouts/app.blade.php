5d3b4cc44404811b9b04a5fae71613fbf14ccda1
afika24@mhs.unitama.ac.id <afika24@mhs.unitama.ac.id>
2026-04-29 16:18:48 +0800

    feat: penyiapan blade component untuk layout utama

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Movie App</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="/">Movie App</a>
+        </div>
+    </nav>
+
+    <div class="container mt-4">
+        @yield('content')
+    </div>
+
+</body>
+
+</html>
