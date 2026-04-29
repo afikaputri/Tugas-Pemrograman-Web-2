cef2e0551b5cfd14c859fa016c36cfbd0bc90c1a
afika24@mhs.unitama.ac.id <afika24@mhs.unitama.ac.id>
2026-04-29 17:29:18 +0800

    feat: implementasi fitur read untuk Movie

---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,80 @@
+@php
+    use Illuminate\Support\Str;
+@endphp
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="d-flex justify-content-between align-items-center mb-4">
+
+        <div>
+            <h1 class="fw-bold"> Movie Collection</h1>
+            <p class="text-muted">
+                Daftar film favorit terbaik pilihan.
+            </p>
+        </div>
+
+        <a href="#" class="btn btn-dark">
+            + Tambah Film
+        </a>
+
+    </div>
+
+    <div class="row">
+
+        @forelse($movies as $movie)
+            <div class="col-md-4 mb-4">
+
+                <div class="card border-0 shadow-sm h-100">
+
+                    <img src="https://via.placeholder.com/400x250" class="card-img-top">
+
+                    <div class="card-body">
+
+                        <div class="d-flex justify-content-between mb-2">
+
+                            <span class="badge bg-dark">
+                                {{ $movie->genre }}
+                            </span>
+
+                            <span class="text-warning fw-bold">
+                                ⭐ {{ $movie->rating }}
+                            </span>
+
+                        </div>
+
+                        <h4 class="fw-bold">
+                            {{ $movie->title }}
+                        </h4>
+
+                        <p class="text-muted mb-1">
+                            Director: {{ $movie->director }}
+                        </p>
+
+                        <p class="text-muted">
+                            Release: {{ $movie->release_year }}
+                        </p>
+
+                        <p>
+                            {{ Str::limit($movie->synopsis, 80) }}
+                        </p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        @empty
+
+            <div class="col-12">
+
+                <div class="alert alert-secondary text-center">
+                    Belum ada data film.
+                </div>
+
+            </div>
+        @endforelse
+
+    </div>
+@endsection
